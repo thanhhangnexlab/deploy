f0f33009fe6f5a0066eff11eca8206488e078c6e
Thanh Hang <thanhhang.tran@cj.net>
2024-04-16 13:46:36 +0700

    update run build

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Dashboard from '../views/Dashboard.tsx'
+import Dashboard from '../views/Dashboard'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
