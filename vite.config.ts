e606375f5b0983a17f5217f8b06c0a66b90bc14b
Thanh Hang <thanhhang.tran@cj.net>
2024-04-16 15:26:05 +0700

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,11 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: '/'
 })
